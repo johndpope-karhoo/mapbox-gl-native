3dbb7aab31f1df28de6a1ab3bc8b925c6f8c64db
Ivo van Dongen <info@ivovandongen.nl>
2016-07-21 18:29:38 +0200

    [android] #5610 - raster layer is not filterable

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/style/layers/RasterLayer.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/style/layers/RasterLayer.java
@@ -1,13 +1,10 @@
 // This file is generated. Edit android/platform/scripts/generate-style-code.js, then run `make android-style-code`.
 package com.mapbox.mapboxsdk.style.layers;
 
-import com.mapbox.mapboxsdk.style.Filter;
-import com.mapbox.mapboxsdk.style.Filterable;
-
 /**
  * Raster Layer
  */
-public class RasterLayer extends Layer implements Filterable {
+public class RasterLayer extends Layer {
 
     public RasterLayer(long nativePtr) {
         super(nativePtr);
@@ -18,15 +15,5 @@ public class RasterLayer extends Layer implements Filterable {
     }
 
     protected native void initialize(String layerId, String sourceId);
-
-    @Override
-    public void setFilter(Filter.Statement filter) {
-        this.setFilter(filter.toArray());
-    }
-
-    @Override
-    public void setFilter(Object[] filter) {
-        nativeSetFilter(filter);
-    }
 
 }
